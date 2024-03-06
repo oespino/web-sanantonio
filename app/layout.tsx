6cc29cfb3164abeb5d6cac814dbf71d3ea9b26b0
oespino <oespino88@gmail.com>
2024-03-06 17:20:11 +0100

    Add JSON-LD metadata for LocalBusiness

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,49 @@ export const viewPort: Viewport = {
 	],
 }
 
+const jsonLd = {
+	'@context': 'https://schema.org/',
+	'@type': 'LocalBusiness',
+	name: siteConfig.name,
+	image: "https://www.avicolasanantonio.com/favicon-16x16.png",
+	description: siteConfig.description,
+	address: {
+		'@type': "PostalAddress",
+		addressCountry: "ES",
+		addressRegion: "Las Palmas",
+		addressLocality: "Ingenio",
+		postalCode: "35250",
+		streetAddress: "Cam. Viejo Al Carrizal, S/N"
+	},
+	geo: {
+		'@type': "GeoCoordinates",
+		latitude: 27.91459182516049,
+		longitude: -15.42733958998171
+	},
+	url: "https://www.avicolasanantonio.com/",
+	telephone: "+34928781329",
+	openingHoursSpecification: [
+		{
+			"@type": "OpeningHoursSpecification",
+			"dayOfWeek": [
+				"Monday",
+				"Tuesday",
+				"Wednesday",
+				"Thursday",
+				"Friday",
+			],
+			"opens": "08:00",
+			"closes": "13:00"
+		},
+		{
+			"@type": "OpeningHoursSpecification",
+			"dayOfWeek": "Saturday",
+			"opens": "08:00",
+			"closes": "11:00"
+		}
+	],
+}
+
 
 export default function RootLayout({
 	children,
@@ -54,6 +97,10 @@ export default function RootLayout({
 				<Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
 					<div className="relative flex flex-col min-h-screen overflow-x-hidden">
 						<Navbar />
+						<script
+							type="application/ld+json"
+							dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+						/>
 						<main className="mx-auto w-screen">
 							{children}
 						</main>
