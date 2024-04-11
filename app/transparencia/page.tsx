27ebf3039fed05cffd9d30128404233ec3da24cf
oespino <oespino88@gmail.com>
2024-04-11 19:50:20 +0200

    Add permalinks

--- app/transparencia/page.tsx
+++ app/transparencia/page.tsx
@@ -19,16 +19,16 @@ export default function TransparenciaPage() {
                 A través del Portal de Transparencia de Granja Avícola San Antonio C.B. puede acceder a toda
                 la información pública de la empresa. Este portal de transparencia se ha creado siguiendo las
                 indicaciones del Comisionado de Transparencia del Gobierno de Canarias y en cumplimiento
-                con la <span className="font-bold">Ley 19/2013, de 9 de diciembre, de transparencia, acceso a la información pública y
-                    buen gobierno</span> y la <span className="font-bold">Ley 12/2014, de 26 de diciembre, de transparencia y de acceso a la
-                        información pública</span>.
+                con la <a target="_blank" href="https://www.boe.es/eli/es/l/2013/12/09/19/con" className="font-bold">Ley 19/2013, de 9 de diciembre, de transparencia, acceso a la información pública y
+                    buen gobierno <ExternalLinkLogo size={16} className="inline mb-1" /></a> y la <a target="_blank" href="https://www.boe.es/eli/es-cn/l/2014/12/26/12" className="font-bold">Ley 12/2014, de 26 de diciembre, de transparencia y de acceso a la
+                        información pública <ExternalLinkLogo size={16} className="inline mb-1" /></a>.
             </p>
             <p className="py-4">
                 Esta información será actualizada bienalmente, para que nuestros clientes, proveedores y la
                 ciudadanía en general puedan consultar la información sobre nuestra empresa.
             </p>
             <p className="py-4">
-                Última actualización: 15 de Abril de 2024.
+                Última actualización: 11 de Abril de 2024.
             </p>
             <p className="py-4">
                 Período: 2022-2023
@@ -43,13 +43,13 @@ export default function TransparenciaPage() {
                 normativas:
             </p>
             <p className="py-4 pl-4">
-                <a target="_blank" href="https://www.boe.es/buscar/act.php?id=BOE-A-1889-4763&amp;tn=1&amp;p=20230301">
+                <a target="_blank" href="https://www.boe.es/eli/es/rd/1889/07/24/(1)/con">
                     <span className="font-bold">· Real Decreto de 24 de julio de 1889 por el que se publica el Código
                         Civil. <ExternalLinkLogo size={16} className="inline mb-2 mx-1" /></span>
                 </a>
             </p>
             <p className="pl-4">
-                <a target="_blank" href="https://www.boe.es/buscar/act.php?id=BOE-A-2013-12887">
+                <a target="_blank" href="https://www.boe.es/eli/es/l/2013/12/09/19/con">
                     <span className="font-bold">· Ley 19/2013, de 9 de diciembre, de transparencia, acceso a la información pública y buen
                         gobierno. <ExternalLinkLogo size={16} className="inline mb-2 mx-1" /></span>
                 </a>
@@ -83,7 +83,7 @@ export default function TransparenciaPage() {
             <h2 id="serviciosyprocedimientos" className={subtitle({ class: "text-green-800 font-bold mt-6" })}>Servicios y procedimientos</h2>
             <p className="py-4">
                 Aunque nuestra empresa no esté obligada a disponer de un canal interno para denuncias
-                según la <a target="_blank" href="https://www.boe.es/buscar/act.php?id=BOE-A-2023-4513" className="font-bold">Ley 2/2023, de 20 de febrero, reguladora de la protección de las personas que
+                según la <a target="_blank" href="https://www.boe.es/eli/es/l/2023/02/20/2/con" className="font-bold">Ley 2/2023, de 20 de febrero, reguladora de la protección de las personas que
                     informen sobre infracciones normativas y de lucha contra la corrupción <ExternalLinkLogo size={16} className="inline mb-1" /></a>,
                 pues no alcanza la cifra de 50 trabajadores, la empresa dispone del correo <a target="_blank" href="mailto:transparencia@avicolasanantonio.com" className="font-bold">transparencia@avicolasanantonio.com</a> como
                 sistema interno de información.
@@ -101,9 +101,9 @@ export default function TransparenciaPage() {
             <h3 className="text-green-800 font-bold text-md">Cuentas anuales de la entidad</h3>
             <p className="py-4">
                 Granja Avícola San Antonio C.B. cumple con sus obligaciones documentales y contables tal y
-                como se establece el artículo 6 de la <a target="_blank" href="https://www.boe.es/buscar/act.php?id=BOE-A-2014-12328&amp;tn=1&amp;p=20240220" className="font-bold">Ley 27/2014, de 27 de noviembre, del Impuesto sobre
+                como se establece el artículo 6 de la <a target="_blank" href="https://www.boe.es/eli/es/l/2014/11/27/27/con" className="font-bold">Ley 27/2014, de 27 de noviembre, del Impuesto sobre
                     Sociedades <ExternalLinkLogo size={16} className="inline mb-1" /></a>,
-                y el artículo 35.4 de la <a target="_blank" href="https://www.boe.es/buscar/act.php?id=BOE-A-2003-23186" className="font-bold">Ley 58/2003, de 17 de diciembre, General Tributaria <ExternalLinkLogo size={16} className="inline mb-1" /></a>.
+                y el artículo 35.4 de la <a target="_blank" href="https://www.boe.es/eli/es/l/2003/12/17/58/con" className="font-bold">Ley 58/2003, de 17 de diciembre, General Tributaria <ExternalLinkLogo size={16} className="inline mb-1" /></a>.
             </p>
 
             <h3 className="text-green-800 font-bold mt-6 text-md">Informes de auditoría de cuentas y de fiscalización por los órganos de
